ad2c748626f222b10d0f3cc2c7e36d403df675f9
The Old Schooler <70700334+The-old-Schooler@users.noreply.github.com>
2020-09-28 09:07:29 -0400

    Update pxt.json, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 """
-Title:
-Creators:
-Description:
+Title:Flip
+Creators:Robert,Aden
+Description:u flip gravity
 """
+player1 = sprites.create(img("""
+    . . . . . . f f f f f f f . . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b 1 1 f 1 f 1 f . .
+    . . . . . f b 1 1 f 1 f 1 f . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b b b b b b b f . .
+    . . . . . . f f f f f f f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 f f 1 f . . .
+    . . . . . . f b b f b b f . . .
+    . . . . . . f f f f f f f . . .
+"""))
+tiles.set_tilemap(tilemap("""level"""))
--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
 /** 
-Title:
-Creators:
-Description:
+Title:Flip
+Creators:Robert,Aden
+Description:u flip gravity
 
  */
+let player1 = sprites.create(img`
+    . . . . . . f f f f f f f . . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b 1 1 f 1 f 1 f . .
+    . . . . . f b 1 1 f 1 f 1 f . .
+    . . . . . f b 1 1 1 1 1 1 f . .
+    . . . . . f b b b b b b b f . .
+    . . . . . . f f f f f f f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 1 1 1 f . . .
+    . . . . . . f b 1 f f 1 f . . .
+    . . . . . . f b b f b b f . . .
+    . . . . . . f f f f f f f . . .
+`)
+tiles.setTilemap(tilemap`level`)
