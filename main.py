988f1d0a84c13ea3425f0be7c0af6189a8170e94
The Old Schooler <70700334+The-old-Schooler@users.noreply.github.com>
2020-09-29 08:45:18 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -3,22 +3,100 @@ Title:Flip
 Creators:Robert,Aden
 Description:u flip gravity
 """
+bpress = 0
+gravity = 0
 player1 = sprites.create(img("""
-    . . . . . . f f f f f f f . . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b 1 1 f 1 f 1 f . .
-    . . . . . f b 1 1 f 1 f 1 f . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b b b b b b b f . .
-    . . . . . . f f f f f f f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 f f 1 f . . .
-    . . . . . . f b b f b b f . . .
-    . . . . . . f f f f f f f . . .
+        . . . . . . f f f f f f f . . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b 1 1 f 1 f 1 f . . 
+            . . . . . f b 1 1 f 1 f 1 f . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b b b b b b b f . . 
+            . . . . . . f f f f f f f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 f f 1 f . . . 
+            . . . . . . f b b f b 1 f . . .
+    """),
+    SpriteKind.player)
+    
+scene.set_tile_map(img("""
+    22222222222222222222..........
+    ......22222222222222..........
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..1111111.....................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    .........111111111111.........
+    111111111222222222222111111111
+    222222222222222222222222222222
 """))
-tiles.set_tilemap(tilemap("""level"""))
+scene.set_tile(1, img("""
+    7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 5 7 5 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e
+    e e e e e 7 7 7 e e e 7 7 e e e
+    e e e e e e e e e e e e e e e e
+    e e c e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e c e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e c e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+"""),True)
+scene.set_tile(2, img("""
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e c e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e c e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e c e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e c e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+"""),True)
+def on_forever():
+    global gravity, bpress
+    player1.vy = gravity
+    scene.camera_follow_sprite(player1)
+    if bpress == 0:
+        gravity = 210
+    else:
+        gravity = -210
+    if controller.B.is_pressed() and player1.is_hitting_tile(CollisionDirection.BOTTOM):
+        bpress = 1
+    if controller.B.is_pressed() and player1.is_hitting_tile(CollisionDirection.TOP):
+        bpress = 0
+        pass
+    if controller.right.is_pressed():
+        player1.vx = 100
+    elif controller.left.is_pressed():
+        player1.vx = -100
+    else:
+        player1.vx = 0
+
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,22 +4,105 @@ Creators:Robert,Aden
 Description:u flip gravity
 
  */
+let bpress = 0
+let gravity = 0
 let player1 = sprites.create(img`
-    . . . . . . f f f f f f f . . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b 1 1 f 1 f 1 f . .
-    . . . . . f b 1 1 f 1 f 1 f . .
-    . . . . . f b 1 1 1 1 1 1 f . .
-    . . . . . f b b b b b b b f . .
-    . . . . . . f f f f f f f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 1 1 1 f . . .
-    . . . . . . f b 1 f f 1 f . . .
-    . . . . . . f b b f b b f . . .
-    . . . . . . f f f f f f f . . .
+        . . . . . . f f f f f f f . . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b 1 1 f 1 f 1 f . . 
+            . . . . . f b 1 1 f 1 f 1 f . . 
+            . . . . . f b 1 1 1 1 1 1 f . . 
+            . . . . . f b b b b b b b f . . 
+            . . . . . . f f f f f f f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 1 1 1 f . . . 
+            . . . . . . f b 1 f f 1 f . . . 
+            . . . . . . f b b f b 1 f . . .
+    `, SpriteKind.Player)
+scene.setTileMap(img`
+    22222222222222222222..........
+    ......22222222222222..........
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..1111111.....................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    ..............................
+    .........111111111111.........
+    111111111222222222222111111111
+    222222222222222222222222222222
 `)
-tiles.setTilemap(tilemap`level`)
+scene.setTile(1, img`
+    7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 5 7 5 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e
+    e e e e e 7 7 7 e e e 7 7 e e e
+    e e e e e e e e e e e e e e e e
+    e e c e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e c e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e c e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+`, true)
+scene.setTile(2, img`
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e c e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e c e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e c e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e c e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+`, true)
+forever(function on_forever() {
+    
+    player1.vy = gravity
+    scene.cameraFollowSprite(player1)
+    if (bpress == 0) {
+        gravity = 210
+    } else {
+        gravity = -210
+    }
+    
+    if (controller.B.isPressed() && player1.isHittingTile(CollisionDirection.Bottom)) {
+        bpress = 1
+    }
+    
+    if (controller.B.isPressed() && player1.isHittingTile(CollisionDirection.Top)) {
+        bpress = 0
+        
+    }
+    
+    if (controller.right.isPressed()) {
+        player1.vx = 100
+    } else if (controller.left.isPressed()) {
+        player1.vx = -100
+    } else {
+        player1.vx = 0
+    }
+    
+})
